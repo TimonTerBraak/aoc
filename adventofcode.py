c6fefd0a8807448c6539511772dbb1789862ad68
ttb <ttb@localhost>
2021-12-02 13:00:26 +0100

    Hardening implementation by type hinting and abstract method declarations.

--- adventofcode.py
+++ adventofcode.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-
+from typing import List, Tuple, Any
 from dataclasses import dataclass
 from contextlib import contextmanager
 import importlib
 import os
 import sys
 import time
-
+import abc
 
 class LineReader:
 
-    def __init__(self, filename: str):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
     @contextmanager
-    def integers(self):
+    def integers(self) -> List[int]:
         try:
             file = open(self.filename, 'r')
             yield [int(i) for i in file.read().split('\n')[:-1]]
@@ -22,18 +22,18 @@ class LineReader:
             file.close()
 
     @contextmanager
-    def strings(self):
+    def strings(self) -> List[str]:
         try:
             file = open(self.filename, 'r')
-            yield [i for i in file.read().split('\n')[:-1]]
+            yield file.read().split('\n')[:-1]
         finally:
             file.close()
 
     @contextmanager
-    def moves(self):
+    def moves(self) -> Tuple[str, int]:
         try:
             file = open(self.filename, 'r')
-            moves = [i for i in file.read().split('\n')[:-1]]
+            moves = file.read().split('\n')[:-1]
             pairs = [tuple(m.split(' ')) for m in moves]
             yield [(m, int(c)) for m, c in pairs]
         finally:
@@ -42,8 +42,9 @@ class LineReader:
 
 class Part:
 
-    def solve(self, inputfile: str):
-        ...
+    @abc.abstractmethod
+    def solve(self, inputfile: str) -> Any:
+        raise NotImplementedError
 
 
 @dataclass
@@ -54,7 +55,7 @@ class Puzzle:
     part1: type
     part2: type
 
-    def __init__(self, year: int, day: int):
+    def __init__(self, year: int, day: int) -> None:
         self.year = year
         self.day = day
 
@@ -62,7 +63,7 @@ class Puzzle:
         self.part1 = getattr(module, 'Part1')
         self.part2 = getattr(module, 'Part2')
 
-    def solve(self):
+    def solve(self) -> None:
         print(f'Puzzle: {self.year}-{self.day}')
         ans = self.part1().solve(os.path.join(self.year, self.day, 'example1.txt'))
         print(f'Ex.  1: {ans}')
@@ -74,8 +75,8 @@ class Puzzle:
         print(f'Part 2: {ans}')
 
 
-def main():
-    if len (sys.argv) < 3:
+def main() -> None:
+    if len(sys.argv) < 3:
         print(f'Usage: {sys.argv[0]} <year> <day>')
     else:
         start = time.time()
@@ -85,4 +86,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- submarine.py
+++ submarine.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import abc
 
 @dataclass
 class Submarine:
@@ -7,23 +8,29 @@ class Submarine:
     y: int = 0
     z: int = 0
 
+    @abc.abstractmethod
     def forward(self, dx: int):
-        ...
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def backward(self, dx: int):
-        ...
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def left(self, dy: int):
-        ...
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def right(self, dy: int):
-        ...
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def up(self, dz: int):
-        ...
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def down(self, dz: int):
-        ...
+        raise NotImplementedError
 
     def move(self, direction: str, distance: int):
         if direction == "forward":
@@ -38,5 +45,3 @@ class Submarine:
             self.down(distance)
         elif direction == "up":
             self.up(distance)
-
-
