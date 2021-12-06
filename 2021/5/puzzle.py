d1ae9dbdcc04c2e62d16dee4d7454f504d41c615
ttb <ttb@localhost>
2021-12-06 11:13:02 +0100

    Solved 2021 day 5 part 1

---
+++ 2021/5/puzzle.py
@@ -0,0 +1,45 @@
+from adventofcode import Part, LineReader
+from parse import parse
+
+class Part1(Part):
+
+    def solve(self, inputfile):
+        with LineReader(inputfile).strings() as lines:
+            segments = []
+            for line in lines:
+                segments.append(list(map(int, parse('{},{} -> {},{}', line))))
+
+            m = [[0 for i in range(1000)] for j in range(1000)]
+            for x0,y0,x1,y1 in segments:
+                if x0 == x1:
+                    if y1 < y0:
+                        y0, y1 = y1, y0
+
+                    for y in range(y0, y1 + 1):
+                        m[y][x0] = m[y][x0] + 1
+
+                if y0 == y1:
+                    if x1 < x0:
+                        x0, x1 = x1, x0
+                    for x in range(x0, x1 + 1):
+                        m[y0][x] = m[y0][x] + 1
+                    #print(f'({x0},{y0}) -> ({x1},{y1})')
+
+            #3941 too low
+            count = 0
+            for row in m:
+                for cell in row:
+                    if cell >= 2:
+                        count = count + 1
+            return count
+
+        return ""
+
+
+class Part2(Part):
+
+    def __init__(self):
+        ...
+
+    def solve(self, inputfile):
+        return ""
